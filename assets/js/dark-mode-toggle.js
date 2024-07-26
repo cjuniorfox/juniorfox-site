fa2b0ed78044898f5c4f215ad030573007248c98
Junior <cjuniorfox@gmail.com>
2024-07-25 22:08:50 -0300

    chore: improved the data toggle

--- assets/js/dark-mode-toggle.js
+++ assets/js/dark-mode-toggle.js
@@ -3,23 +3,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.getElementById('toggle-dark-mode')
   const currentTheme = localStorage.getItem('theme')
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+  const lightThemeLink = document.getElementById('hljs-light-theme')
+  const darkThemeLink = document.getElementById('hljs-dark-theme')
 
+  let darkMode = false
   if (currentTheme) {
-    document.body.classList.add(currentTheme)
-    toggleButton.checked = currentTheme === 'dark-mode'
-  } else if (prefersDarkScheme.matches) {
-    document.body.classList.add('dark-mode')
-    toggleButton.checked = true
+    darkMode = currentTheme === 'dark-mode'
   } else {
-    toggleButton.checked = false
+    darkMode = prefersDarkScheme.matches
   }
+
+  if (darkMode){
+    document.body.classList.add('dark-mode')
+  } else {
+    document.body.classList.add('light-mode')
+  }
+  toggleButton.checked = darkMode
+  darkThemeLink.disabled = !darkMode
+  lightThemeLink.disabled = darkMode
+  
 
   toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode')
-    let theme = 'light'
+    let theme = 'light-mode'
     if (document.body.classList.contains('dark-mode')) {
       theme = 'dark-mode'
     }
+    darkThemeLink.disabled = theme === 'light-mode'
+    lightThemeLink.disabled = theme === 'dark-mode'
     localStorage.setItem('theme', theme)
   });
 });
