f82cd6a79fbc6ab88c3c2b09b3406afe87b9be21
Junior <cjuniorfox@gmail.com>
2024-09-13 18:39:21 -0300

    development

--- kubernetes/create_secret.sh
+++ kubernetes/create_secret.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-export MONGO_INITDB_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)"
-export MONGO_USER="juniorfox"
-export MONGO_PASS="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)"
-export MONGO_DB="juniorfoxsite"
+export MONGO_INITDB_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})"
+export MONGO_USER='juniorfox'
+export MONGO_PASS="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})"
+export MONGO_DB='juniorfoxsite'
 
 cat <<EOF > secrets.json
 {
-  "mongoUser": "$(echo ${MONGO_USER} | base64)",
-  "mongoDatabase": "$(echo ${MONGO_DB} | base64)",
-  "mongoRootPassword": "$(echo -n ${MONGO_INITDB_PASSWORD} | base64)",
-  "mongoPassword": "$(echo -n ${MONGO_PASS} | base64)"
+  "mongoUser": "$(echo -n ${MONGO_USER} | base64 -w 0)",
+  "mongoDatabase": "$(echo -n ${MONGO_DB} | base64 -w 0)",
+  "mongoRootPassword": "$(echo -n ${MONGO_INITDB_PASSWORD} | base64 -w 0)",
+  "mongoPassword": "$(echo -n ${MONGO_PASS} | base64 -w 0)"
 }
 EOF
 
 echo "Secrets generated and saved to secrets.json"
-
 
 cat << EOF > secrets.yaml
 apiVersion: v1
@@ -23,10 +22,10 @@ kind: Secret
 metadata:
   name: juniorfox-site-secrets
 data:
-  mongoUser: $(echo ${MONGO_USER} | base64)
-  mongoDatabase: $(echo ${MONGO_DB} | base64)
-  mongoRootPassword: $(echo -n ${MONGO_INITDB_PASSWORD} | base64)
-  mongoPassword: $(echo -n ${MONGO_PASS} | base64)
+  mongoUser: $(echo -n ${MONGO_USER} | base64 -w 0)
+  mongoDatabase: $(echo -n ${MONGO_DB} | base64 -w 0)
+  mongoRootPassword: $(echo -n ${MONGO_INITDB_PASSWORD} | base64 -w 0)
+  mongoPassword: $(echo -n ${MONGO_PASS} | base64 -w 0)
 EOF
 
 echo "Secret file created with the name secrets.yaml"
