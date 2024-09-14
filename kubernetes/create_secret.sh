fd515193a22b72c33d6d10ef6438d93c59e38e33
Junior_FOX <cjuniorfox@gmail.com>
2024-09-13 23:13:08 -0300

    chore: renamed the environment variable

--- kubernetes/create_secret.sh
+++ kubernetes/create_secret.sh
@@ -3,12 +3,12 @@
 export MONGO_INITDB_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})"
 export MONGO_USER='juniorfox'
 export MONGO_PASS="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})"
-export MONGO_DB='juniorfoxsite'
+export MONGO_DBNAME='juniorfoxsite'
 
 cat <<EOF > secrets.json
 {
   "mongoUser": "$(echo -n ${MONGO_USER} | base64 -w 0)",
-  "mongoDatabase": "$(echo -n ${MONGO_DB} | base64 -w 0)",
+  "mongoDatabase": "$(echo -n ${MONGO_DBNAME} | base64 -w 0)",
   "mongoRootPassword": "$(echo -n ${MONGO_INITDB_PASSWORD} | base64 -w 0)",
   "mongoPassword": "$(echo -n ${MONGO_PASS} | base64 -w 0)"
 }
@@ -23,7 +23,7 @@ metadata:
   name: juniorfox-site-secrets
 data:
   mongoUser: $(echo -n ${MONGO_USER} | base64 -w 0)
-  mongoDatabase: $(echo -n ${MONGO_DB} | base64 -w 0)
+  mongoDatabase: $(echo -n ${MONGO_DBNAME} | base64 -w 0)
   mongoRootPassword: $(echo -n ${MONGO_INITDB_PASSWORD} | base64 -w 0)
   mongoPassword: $(echo -n ${MONGO_PASS} | base64 -w 0)
 EOF
