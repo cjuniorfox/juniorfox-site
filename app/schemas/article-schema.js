30b32d1ab2b8ebdd7ebef34b91a55b7cfcd395f7
Junior <cjuniorfox@gmail.com>
2024-09-17 18:59:25 -0300

    chore: register articles in the database

---
+++ app/schemas/article-schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    title: String,
+    articleId: String,
+    date: Date,
+    author: String,
+    category: String,
+    brief: String,
+    image: String,
+    keywords: [String],
+    lang: String,
+    otherLangs: [{ lang: String, article: String }],
+    readingTime: String,
+    removed: { type: Boolean, default: false }, // Flag for removed articles
+    updatedAt: { type: Date, default: Date.now }
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
---
+++ app/services/article-service.js
@@ -0,0 +1,84 @@
+const Article = require('../schemas/article-schema');
+const getArticlesFromFilesystem = require('../utils/get-articles-from-filesystem');
+const path = require("path");
+const fs = require('fs');
+const matter = require('gray-matter');
+
+const __getFilePath = (articleName) => {
+    return path.join(__dirname, '..', 'articles', `${articleName}.md`);
+}
+
+const articleFileExists = async (articleName) => {
+    const filePath = __getFilePath(articleName);
+    try {
+        await fs.promises.access(filePath, fs.constants.F_OK);
+        return true
+    } catch (err) {
+        return false;
+    }
+}
+
+const article = async (articleName, locale) => {
+    const filePath = __getFilePath(articleName);
+    const { data: metadata, content } = matter(await fs.promises.readFile(filePath, 'utf8'));
+
+    if (metadata.lang && metadata.lang != locale && metadata['other-langs']) {
+        const otherLangs = metadata['other-langs'];
+        const matchingLang = otherLangs.find(langObj => langObj.lang === locale);
+        if (matchingLang) {
+            return { redirect: `/article/${matchingLang.article}` }
+        }
+    }
+
+    return { articleName, content, ...metadata }
+}
+
+const getArticlesList = async (lang, page = 1, limit = 10) => {
+    const skip = (page - 1) * limit;
+    const articles = await Article.find({ lang: lang, removed: false })
+        .sort({ date: -1 }) // Sort by date in descending order
+        .skip(skip)
+        .limit(limit);
+    const totalArticles = await Article.countDocuments({ lang: lang, removed: false });
+
+    return {
+        articles,
+        totalArticles,
+        currentPage: page,
+        totalPages: Math.ceil(totalArticles / limit)
+    };
+}
+
+const syncDatabase = async () => {
+    const articlesFromFileSystem = getArticlesFromFilesystem();
+    const articlesFromDB = await Article.find();
+    const articlesMap = new Map();
+    articlesFromDB.forEach(article => {
+        articlesMap.set(article.name, article);
+    });
+    const processedArticles = new Set();
+
+    for (const article of articlesFromFileSystem) {
+        const existingArticle = articlesMap.get(article.name);
+
+        if (existingArticle) {
+            const isUpdated = existingArticle.updatedAt < new Date(article.date);
+            if (isUpdated) {
+                // Update the article in the database
+                await Article.updateOne({ name: article.name }, { ...article, removed: false, updatedAt: new Date() });
+            }
+        } else {
+            await Article.create(article);
+        }
+
+        processedArticles.add(article.name);
+    }
+
+    for (const article of articlesFromDB) {
+        if (!processedArticles.has(article.name)) {
+            await Article.updateOne({ name: article.name }, { removed: true });
+        }
+    }
+}
+
+module.exports = { articleFileExists, article, getArticlesList, syncDatabase };
